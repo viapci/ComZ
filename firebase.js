7219e50adfc2e164c31b013ba7c7f46235d1d8bc
viapci <repzal69@gmail.com>
2025-06-14 13:50:17 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,48 @@
+// firebase.js (must be a type="module" script)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  onSnapshot,
+  query,
+  where,
+  getDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA40CvBmy-kLiOwqQaXKhKeMuTXJJvJ1vk",
+  authDomain: "comz-d46bf.firebaseapp.com",
+  projectId: "comz-d46bf",
+  storageBucket: "comz-d46bf.appspot.com",
+  messagingSenderId: "241012409719",
+  appId: "1:241012409719:web:8461309ccca50734dd0d72",
+  measurementId: "G-P6YVXNYH0F"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
+
+// Sign in anonymously
+signInAnonymously(auth).catch(console.error);
+
+export {
+  auth,
+  db,
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  onSnapshot,
+  query,
+  where,
+  getDoc,
+  serverTimestamp
+};
